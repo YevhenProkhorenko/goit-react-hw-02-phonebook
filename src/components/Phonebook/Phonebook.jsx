09a571b7b114f21984041502b21b5d1953f76d96
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-09-18 14:00:34 +0300

    taking the form into a separate component

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,29 +1,25 @@
-import { nanoid } from 'nanoid';
 import { Component } from 'react';
+import ContactForm from './ContactForm';
 import css from '../Phonebook/Phonebook.module.css';
 
 export default class Phonebook extends Component {
   state = {
     contacts: [],
-    name: '',
   };
+
+  addConctact = data => {
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, data],
+      };
+    });
+  };
+
   render() {
+    const { addConctact } = this;
     return (
-      <div className={css.formWrapper}>
-        <label htmlFor="" className={css.labelText}>
-          Name
-        </label>
-        <input
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          className={css.inputStyles}
-        />
-        <button type="submit" className={css.submitButton}>
-          Add contact
-        </button>
+      <div className={css.mainWrapper}>
+        <ContactForm onSubmit={addConctact} />
       </div>
     );
   }
